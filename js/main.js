45fd76310c71a5b26483f90ca49b7d2f6d6bcd2b
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-08-24 23:53:16 +0500

    footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         dots: false,
         margin: 48,
         loop: true,
     });
-  });
+});
+
+
+//   VIDEO
+
+const videoBtn = document.querySelector('#video-btn');
+const videoPicture = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+videoWrapper.addEventListener('click', function () {
+
+    if (video.paused) {
+        videoPicture.classList.add('none');
+        videoWrapper.classList.remove('video-overlay');
+        videoBtn.classList.add('none');
+        video.play()
+    } else {
+        video.pause();
+        videoBtn.classList.remove('none');
+        videoWrapper.classList.add('video-overlay');
+    }
+})
